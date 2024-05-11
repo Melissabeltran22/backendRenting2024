147941e9172b44a19f7988dc3be8f930bcac2e77
Meli Beltran <meli.cesde@gmail.com>
2024-05-11 14:18:09 -0500

    HistoriadeUsuario1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.example.modelos.Vehiculo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Dentro del main uso las clases del programa
+        //¿como se usa una clase? es asignarle valores a sus atributos . las clases se usan en el main
+        //usar una clase es llamar sus métodos. eso es instanciar una clase
+        //para usar una clase debo crear un objeto
+        //¿que es un objeto? es una variable especial  ejemplo. variable no es especial : edad
+
+        Integer edad = 34;
+        // el tipo de dato de un objeto es el nombre de tu clase, siempre empieza en mayuscula
+
+        //necesito un constructor que es una funcion o metodo especial de la clase que permite la construccion del objeto:
+
+        Vehiculo objeto;
+
+    }
+
+
+}
---
+++ src/main/java/org/example/modelos/Mercancia.java
@@ -0,0 +1,31 @@
+package org.example.modelos;
+
+public class Mercancia {
+    //id int
+    Integer id;
+
+    //volumen double
+    Double volumen;
+
+    //tipo string
+    String Tipo;
+
+    //peso double
+    Double Peso;
+
+    //descripcion strisng
+    String descripcion;
+
+    //avaluo int
+    Integer avaluo;
+
+    //direccionRemitente string
+    String direccionRemitente;
+
+
+    //direccionDestinatario string
+    String direccionDestinatario;
+
+}
+
+
